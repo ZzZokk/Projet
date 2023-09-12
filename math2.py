080d46ed232eae6f28b359c50e754cb5500e413e
j <jltests@protonmail.com>
2023-09-12 15:12:27 +0200

    le boulot est fait les gars

---
+++ math2.py
@@ -0,0 +1,6 @@
+print("Opération Disponibles 
+print("1. Addition")
+print("2. Multiplication")
+print("3. Substraction")
+print("4. Division")
+choix = input("Choisissez l'opération voulu (1, 2, 3, 4,) 

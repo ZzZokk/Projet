684d1b4fddd8f31ac108a811b1206f52d9b98423
ZzZok <zzokk8016@gmail.com>
2023-09-12 14:42:18 +0200

    maj14h42

---
+++ math.py
@@ -0,0 +1,17 @@
+def addition(x, y):
+    return x + y
+
+def multiplication(x, y):
+    return x * y
+
+def substraction(x, y):
+    return x - y
+
+def division(x, y):
+    if y==0:
+            return "division impossible"
+    return x / y
+
+nombre1=float(input("entrez la première valeur : "))
+
+nombre2=float(input("entrez la première valeur : "))
